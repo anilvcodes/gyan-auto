a4a4c2b4d071c08ce67b0a47a2dcf905b70a6b44
Dev-Aniweb <anilkumarve51@gmail.com>
2026-07-20 18:08:08 +0530

    change '

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Gyan Auto Backend Running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
